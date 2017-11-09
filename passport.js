8872a0f6ef14e78edd3ba8bf0dca0211facf4193
amatranga <amatranga892@gmail.com>
2017-11-08 16:26:41 -0800

    Implement routes

--- passport.js
+++ passport.js
@@ -1,9 +1,9 @@
-const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy;
-const { ExtractJwt } = require('passport-jwt');
-const LocalStrategy = require('passport-local').Strategy;
-const { JWT_SECRET } = require('./config/index');
-const User = require('./models/user');
+const passport = require('passport')
+const JwtStrategy = require('passport-jwt').Strategy
+const { ExtractJwt } = require('passport-jwt')
+const LocalStrategy = require('passport-local').Strategy
+const { JWT_SECRET } = require('./config/index')
+const User = require('./models/user')
 
 /*
 Creates a new instance of JwtStrategy.
@@ -17,15 +17,16 @@ After that, a user is looked for. If no user is found, an error is returned
 */
 
 passport.use(new JwtStrategy({
-  jwtFormRequest: ExtractJwt.fromHeader('authorization'),
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: JWT_SECRET
-}, async(payload, done) => {
+}, async (payload, done) => {
   try {
     const user = User.findById(payload.sub)
-
+ 
     if (!user) {
       return done(null, false)
     }
+ 
     done(null, user)
   } catch(err) {
     done(err, false)
@@ -34,21 +35,25 @@ passport.use(new JwtStrategy({
 
 passport.use(new LocalStrategy({
   usernameField: 'email'
-}, async(email, password, done) => {
+}, async (email, password, done) => {
+ 
   try {
     const user = await User.findOne({ email })
-
-    if (!user) {
-      done(null, false)
-    }
-
-    const isMatch = await user.isValidPassword(password)
-
-    if(!isMatch) {
-      return done(null, false)
-    }
-    done(null, user)
-  } catch(error) {
+     
+      if (!user) {
+        return done(null, false)
+      }
+     
+      const isMatch = await user.isValidPassword(password)
+     
+      if (!isMatch) {
+        return done(null, false)
+      }
+     
+      done(null, user)
+ 
+  } catch (error) {
     done(error, false)
   }
+   
 }))
